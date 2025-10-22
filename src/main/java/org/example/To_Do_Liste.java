7ea688fc9f8602635a89d35eecda3378f24b1f0f
Larina <rehhornl@bzz.ch>
2025-10-22 09:42:43 +0200

    first

--- src/main/java/org/example/To_Do_Liste.java
+++ src/main/java/org/example/To_Do_Liste.java
@@ -1,5 +1,14 @@
 package org.example;
 
+    import java.util.ArrayList;
+import java.util.Scanner;
 public class To_Do_Liste {
 
+        // Liste für Aufgaben
+        private static ArrayList<String> aufgaben = new ArrayList<>();
+        private static Scanner scanner = new Scanner(System.in);
+
+        public static void main(String[] args) {
+
+    }
 }
