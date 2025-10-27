0c1e838ba70ebab636329908d204b93687a631cb
Larina <rehhornl@bzz.ch>
2025-10-27 11:26:19 +0100

    finish tests

--- src/test/java/org/example/To_Do_ListeTest.java
+++ src/test/java/org/example/To_Do_ListeTest.java
@@ -10,6 +10,7 @@ class To_Do_ListeTest {
 
     @BeforeEach
     void setUp() {
+        To_Do_Liste.aufgaben.clear();
     }
 
     @AfterEach
@@ -21,19 +22,42 @@ class To_Do_ListeTest {
     }
 
     @Test
-    void addToListe() {
-    }
+    void addToListe(){
+        To_Do_Liste.addToListe("Pizza");
+        assertTrue(To_Do_Liste.aufgaben.contains("Pizza"));
+        To_Do_Liste.addToListe("Hallo");
+        assertTrue(To_Do_Liste.aufgaben.contains("Hallo"));
+        }
+
 
     @Test
     void showAll() {
+
     }
 
     @Test
     void edit() {
+        To_Do_Liste.addToListe("Aufgabe 1");
+        To_Do_Liste.addToListe("Aufgabe 2");
+        To_Do_Liste.edit(1, "Aufgabe 2 - geändert");
+        assertEquals("Aufgabe 2 - geändert", To_Do_Liste.aufgaben.get(1));
+        assertEquals("Aufgabe 1", To_Do_Liste.aufgaben.get(0));
+
     }
 
     @Test
     void delet() {
+
+
+        To_Do_Liste.addToListe("Aufgabe 1");
+        To_Do_Liste.addToListe("Aufgabe 2");
+        To_Do_Liste.delete(0);
+        assertFalse(To_Do_Liste.aufgaben.contains("Aufgabe 1"));
+        assertTrue(To_Do_Liste.aufgaben.contains("Aufgabe 2"));
+        assertEquals(1, To_Do_Liste.aufgaben.size());
+        
+
+
     }
 
     @Test
